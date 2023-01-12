385126c19a320f46b1b3430363dfdbd52558fd35
Exist95 <jyeol0210@naver.com>
2023-01-12 20:11:05 +0900

    Constant Variables

---
+++ Constant Variables.dart
@@ -0,0 +1,8 @@
+void main() {
+  //const name = "tom"; 컴파일 시점에 바뀌지 않는 값
+  //final username = fetchAPI(); 컴파일 시점에 바뀌는 값
+}
+
+
+//const: 컴파일 시점에 바뀌지 않는 값 (상수)
+//final: 컴파일 시점에 바뀌는 값 (API에서 받아온 값, 사용자 입력값)
